f7d6bc7a7b0834258ae9dc3e8ab09d4cfc1538ee
1755516802 <zh311505@gmail.com>
2025-06-16 03:17:41 -0500

    feat: configs products

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
+import { API_BASE_URL } from './core/tokens/api-url.token';
+import { environment } from '../environments/environment';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideRouter(routes),
+    { provide: API_BASE_URL, useValue: environment.apiBaseUrl }
+  ]
 };
